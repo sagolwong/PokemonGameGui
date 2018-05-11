94f6804557a2c566faf239170ab3f59b4745ff7c
sagolwong <5910110678@psu.ac.th>
2018-05-12 00:24:50 +0700

    add wantScene

--- src/CommandGame.java
+++ src/CommandGame.java
@@ -2,16 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.nio.ByteOrder;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 public class CommandGame extends JFrame {
     Container c = getContentPane();
     JPanel bg = new JPanel();
+    Icon player;
     public CommandGame(){
         super("POKEMON(demo)");
 
         //c.setLayout(new BorderLayout());
+        bg.setLayout(new BorderLayout());
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1200,700);
@@ -32,6 +35,7 @@ public class CommandGame extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 p1.remove(logoIcon);
                 p2.remove(startButton);
+                p2.remove(quitButton);
                 c.remove(p1);
                 c.remove(p2);
                 selectGender();
@@ -57,9 +61,8 @@ public class CommandGame extends JFrame {
     public void selectGender(){
         JPanel p1 = new JPanel();
         JPanel p2 = new JPanel();
-        bg.setLayout(new BorderLayout());
-        JPanel bg2 = new JPanel();
-        bg2.setLayout(new FlowLayout());
+        JPanel p3 = new JPanel();
+        p3.setLayout(new FlowLayout());
         JLabel text = new JLabel("Select Gender");
         Icon man = new ImageIcon(getClass().getResource("man.png"));
         Icon girl = new ImageIcon(getClass().getResource("girl.png"));
@@ -71,23 +74,56 @@ public class CommandGame extends JFrame {
         manButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                bg.remove(text);
+                bg.remove(p3);
+                wantScene(man);
             }
         });
         girlButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                bg.remove(text);
+                bg.remove(p3);
+                wantScene(girl);
             }
         });
 
         text.setHorizontalAlignment ( SwingConstants.CENTER );
+        p3.setBackground(Color.GRAY);
         p1.add(manButton);
         p2.add(girlButton);
-        bg2.add(p1);
-        bg2.add(p2);
+        p3.add(p1);
+        p3.add(p2);
         bg.add(text,BorderLayout.NORTH);
-        bg.add(bg2,BorderLayout.CENTER);
+        bg.add(p3,BorderLayout.CENTER);
+        c.add(bg);
+
+        setVisible(true);
+    }
+    public void wantScene(Icon player){
+        this.player = player;
+        JPanel p1 = new JPanel();
+        JPanel p2 = new JPanel();
+        JPanel p3 = new JPanel();
+        JLabel img = new JLabel(player);
+        p2.setLayout(new FlowLayout());
+        p3.setLayout(new BoxLayout(p3,BoxLayout.Y_AXIS));
+        Icon dialogtext = new ImageIcon(getClass().getResource(""));
+        JButton shopButton = new JButton("SHOP");
+        JButton wildButton = new JButton("WILD");
+        JButton farmButton = new JButton("FARM");
+        JLabel dialog = new JLabel(dialogtext);
+        dialog.setIcon(dialogtext);
+        img.setIcon(player);
+
+        p1.add(img);
+        p3.add(shopButton);
+        p3.add(wildButton);
+        p3.add(farmButton);
+        p2.add(p1);
+        p2.add(p3);
+        bg.add(p2, BorderLayout.CENTER);
+        bg.add(dialog,BorderLayout.SOUTH);
         c.add(bg);
 
         setVisible(true);
